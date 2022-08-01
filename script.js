29ce8f44ab0c7924e2a17723cbec748353c0ba64
Maxim <bmw_077@mail.ru>
2022-08-01 23:58:20 +0300

    store

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const link = "http://api.weatherstack.com/current?access_key=bb9b12e131244d05701e24f6c41f5185";
 
-const store = {
-  city: 'London'
+let store = {
+  city: 'London',
+  feelslike: 0,
+  cloudcover: 0,
+  temperature: 0,
+  humidity: 0,
+  observationTime: "08:39 AM",
+  pressure: 0,
+  uvIndex: 0,
+  visibility: 0,
+  isDay: 'yes',
+  descriptions: '',
+  wind_speed: 0
 }
 
 const fetchData = async() => {
@@ -9,11 +20,33 @@ const fetchData = async() => {
     const data = await result.json();
 
     const {
-      current: { feelslike, cloudcover, temperature },
-      location: { name }
+      current: { 
+        feelslike, 
+        cloudcover, 
+        temperature, 
+        observation_time: observationTime, 
+        pressure, 
+        uv_index: uvIndex, 
+        visibility,
+        is_day: isDay,
+      },
     } = data;
 
-    console.log(temperature);
+    store = {
+      ...store,
+      feelslike,
+      cloudcover,
+      temperature,
+      observationTime,
+      pressure,
+      uvIndex,
+      visibility,
+      isDay,
+      descriptions: '',
+      wind_speed: 0
+    }
+
+    console.log(data);
 }
 
 fetchData();
